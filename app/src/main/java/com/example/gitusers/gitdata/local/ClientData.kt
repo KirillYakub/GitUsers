75f92d0c63f9964d851b3f57552444f35dab4179
KirillYakub <kirillyakubchinskiy@gmail.com>
2023-08-24 20:20:30 +0300

    Stable 1.0

--- app/src/main/java/com/example/gitusers/gitdata/local/ClientData.kt
+++ app/src/main/java/com/example/gitusers/gitdata/local/ClientData.kt
@@ -1,8 +1,2 @@
 package com.example.gitusers.gitdata.local
 
-object ClientData {
-    const val clientId = "58d0ac669b3daccffb83"
-    const val redirectUrl = "gitapi://callback"
-    const val clientSecret = "bfc83d05f9c28a82ebeda90851d20d1cb53e9eab"
-    const val apiKey = "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"
-}
